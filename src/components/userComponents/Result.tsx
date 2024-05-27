efccd655fc050b4c271ef2ab57cb2376ce0c2680
unknown <thumpel81@gmail.com>
2024-05-27 16:32:54 +0100

    add reload

--- src/components/userComponents/Result.tsx
+++ src/components/userComponents/Result.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 interface ResultProps {
   correctAnswers: number;
   totalQuestions: number;
 }
 
-const Result: React.FC<ResultProps> = ({ correctAnswers, totalQuestions }) => {
+export default function Result({ correctAnswers, totalQuestions }: ResultProps) {
+  const navigate = useNavigate();
+  const handleTryAgain = () => {
+    navigate("/quiz-app/"),{ replace: true };
+    window.location.reload();
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="flex flex-col items-center p-4 max-w-xl w-full mx-auto bg-white rounded shadow-md">
         <h2 className="text-2xl font-bold mb-4 text-center">
           You guessed {correctAnswers} out of {totalQuestions} answers
         </h2>
-        <Link to="/quiz-app/">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleTryAgain}>
             try again
           </button>
-        </Link>
       </div>
     </div>
   );
 };
 
-export default Result;
+
